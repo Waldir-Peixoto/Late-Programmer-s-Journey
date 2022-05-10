ce244bb506f2b28b7d21a4d657183a3120d6c0c8
Waldir.Peixoto <waldir.peixoto@gmail.com>
2022-05-09 21:53:39 -0300

    fundamentos de javascript

---
+++ Anotações dos Estudos/script.js
@@ -0,0 +1,12 @@
+// tipos primitivos
+//boolean
+var VouF = false;
+console.log (typeof(VouF));
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//string
+var nome = 'diana';
+console.log(typeof(nome));
